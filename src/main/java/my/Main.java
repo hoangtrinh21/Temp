0467cdee141b69b7354b0b5d65670a20031b0059
hoangt <hoangtrinh@admicro.vn>
2022-06-02 15:54:14 +0700

    first

--- src/main/java/my/Main.java
+++ src/main/java/my/Main.java
@@ -1,4 +1,13 @@
 package my;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
 }
