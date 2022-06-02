fe8cc89a0907c34d4a09aa7309628e4a83978541
hoangt <hoangtrinh@admicro.vn>
2022-06-02 16:54:34 +0700

    first

--- src/main/java/my/Main.java
+++ src/main/java/my/Main.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        Integer n = Integer.parseInt(args[0]);
         for (int i = 0; i < n; i++) {
             System.out.println(i);
         }
